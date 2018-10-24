29e2324f41adcc33ff516279715b2c1c7e73a688
Luis Gonzalez <luisarmando1234@gmail.com>
2018-10-24 14:20:16 -0500

    update

---
+++ src/ExcelHelper.php
@@ -0,0 +1,155 @@
+<?php
+namespace inquid\yiireports;
+
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use yii\helpers\Html;
+use yii\helpers\Json;
+
+class ExcelHelper
+{
+    private $objPHPExcel;
+    private $addFilters = true;
+    const DEFAULT_PATH = 'files';
+
+    /**
+     * @param array $baseArray Array with the contents
+     * @param array $headers Array of arrays of the headers
+     * [
+     * ['coordinate' => 'A1', 'title' => 'Header 1'],
+     * ['coordinate' => 'B1', 'title' => 'Header 2'],
+     * ['coordinate' => 'C1', 'title' => 'Header 3'],
+     * ['coordinate' => 'D1', 'title' => 'Header 4'],
+     * ['coordinate' => 'E1', 'title' => 'Header 5'],
+     * ]
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function createExportTable($baseArray, $headers)
+    {
+        $lastColumn = $this->getNameFromNumber(count($headers));
+        $this->objPHPExcel = new Spreadsheet();
+        $this->objPHPExcel->setActiveSheetIndex(0);
+        $this->objPHPExcel->getActiveSheet()->getStyle("A1:{$lastColumn}" . (count($baseArray) + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $this->setColumnsStyle();
+        $this->setHeadersStyle("A1:{$lastColumn}1");
+        $this->setHeaders($headers);
+        if ($this->addFilters) {
+            $this->objPHPExcel->getActiveSheet()->setAutoFilter("A1:{$lastColumn}" . (count($baseArray) + 1));
+        }
+        $this->objPHPExcel->getActiveSheet()->fromArray($baseArray, '', 'A2');
+    }
+
+    /**
+     * @param $baseArray
+     * @param $headers
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function createExportTableHorizontal($baseArray, $headers)
+    {
+        $lastColumn = $this->getNameFromNumber(count($headers));
+        $this->objPHPExcel = new Spreadsheet();
+        $this->objPHPExcel->setActiveSheetIndex(0);
+        $this->objPHPExcel->getActiveSheet()->getStyle("A1:{$lastColumn}" . (count($baseArray) + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $this->setColumnsStyle();
+        $this->setHeadersStyle("A1:{$lastColumn}1");
+        $this->setHeaders($headers);
+        if ($this->addFilters) {
+            $this->objPHPExcel->getActiveSheet()->setAutoFilter("A1:{$lastColumn}" . (count($baseArray) + 1));
+        }
+        $this->objPHPExcel->getActiveSheet()->fromArray($baseArray, '', 'A2');
+    }
+
+    /**
+     * @param $range
+     * @param int $size
+     * @param bool $bold
+     * @param string $font_color
+     * @param string $fill_type
+     * @param string $start_color
+     * @param string $end_color
+     */
+    private function setHeadersStyle($range, $size = 14, $bold = true, $font_color = 'FFFFFF', $fill_type = 'solid', $start_color = '', $end_color = '')
+    {
+        $this->objPHPExcel->getActiveSheet()->getStyle($range)->getFont()->setSize($size);
+        $this->objPHPExcel->getActiveSheet()->getStyle($range)->getFont()->setBold($bold);
+        $this->objPHPExcel->getActiveSheet()->getStyle($range)->getFont()->setColor(new Color($font_color));
+        $this->objPHPExcel->getActiveSheet()->getStyle($range)->getFill()->setFillType($fill_type);
+        $this->objPHPExcel->getActiveSheet()->getStyle($range)->getFill()->getStartColor()->setARGB($start_color);
+        $this->objPHPExcel->getActiveSheet()->getStyle($range)->getFill()->getEndColor()->setARGB($end_color);
+    }
+
+    /**
+     * @param array $coordinates_titles
+     */
+    private function setHeaders(array $coordinates_titles)
+    {
+        foreach ($coordinates_titles as $coordinates_title) {
+            $this->objPHPExcel->getActiveSheet()->SetCellValue($coordinates_title['coordinate'], $coordinates_title['title']);
+        }
+    }
+
+    /**
+     */
+    private function setColumnsStyle()
+    {
+        $this->objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+        $this->objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+        $this->objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+    }
+
+
+    public function hideColumns(array $array)
+    {
+        foreach ($array as $item) {
+            $this->objPHPExcel->getActiveSheet()->getColumnDimension($item)->setVisible(false);
+        }
+    }
+
+    public function autoSizeColumns(array $array)
+    {
+        foreach ($array as $item) {
+            $this->objPHPExcel->getActiveSheet()->getColumnDimension($item)->setAutoSize(true);
+        }
+    }
+
+    /**
+     * @param $path
+     * @param $fileName
+     * @return bool|string
+     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     */
+    public function saveExcel($path, $fileName)
+    {
+        $objWriter = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($this->objPHPExcel, 'Xlsx');
+        $objWriter->save("{$path}/{$fileName}.xlsx");
+        return \Yii::getAlias("@web/{$path}/{$fileName}.xlsx");
+    }
+
+    /**
+     * @param $num
+     * @return string
+     */
+    public function getNameFromNumber($num)
+    {
+        $numeric = ($num - 1) % 26;
+        $letter = chr(65 + $numeric);
+        $num2 = (int)(($num - 1) / 26);
+        return $num2 > 0 ? $this->getNameFromNumber($num2) . $letter : $letter;
+    }
+
+    /**
+     * @param $fileName
+     * @param null $title
+     * @param null $path
+     * @param null $class
+     * @return string
+     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     */
+    public function getDownloadButton($fileName, $title = null, $path = null, $class = null)
+    {
+        return Html::a($title !== null ? $title : $fileName, $this->saveExcel($path !== null ? $path : self::DEFAULT_PATH, $fileName), ['class' => $class !== null ? $class : 'btn btn-primary']);
+    }
+
+}
---
+++ src/PDF_HTML.php
@@ -0,0 +1,339 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macbook
+ * Date: 2/19/18
+ * Time: 2:03 PM
+ */
+
+namespace inquid\yiireports;
+
+
+use inquid\pdf\FPDF;
+
+/************************************/
+/* global functions                 */
+/************************************/
+function hex2dec($color = "#000000")
+{
+    $tbl_color = array();
+    $tbl_color['R'] = hexdec(substr($color, 1, 2));
+    $tbl_color['G'] = hexdec(substr($color, 3, 2));
+    $tbl_color['B'] = hexdec(substr($color, 5, 2));
+    return $tbl_color;
+}
+
+function px2mm($px)
+{
+    return $px * 25.4 / 72;
+}
+
+function txtentities($html)
+{
+    $trans = get_html_translation_table(HTML_ENTITIES);
+    $trans = array_flip($trans);
+    return strtr($html, $trans);
+}
+
+
+class PDF_HTML extends FPDF
+{
+    protected $B;
+    protected $I;
+    protected $U;
+    protected $HREF;
+    protected $fontList;
+    protected $issetfont;
+    protected $issetcolor;
+    public $debug = false;
+    public $PRE = false;
+
+    function WriteHTML($html, $bi)
+    {
+        //remove all unsupported tags
+        $this->bi = $bi;
+        if ($bi)
+            $html = strip_tags($html, "<a><img><p><br><font><tr><blockquote><h1><h2><h3><h4><pre><red><blue><ul><li><hr><b><i><u><strong><em>");
+        else
+            $html = strip_tags($html, "<a><img><p><br><font><tr><blockquote><h1><h2><h3><h4><pre><red><blue><ul><li><hr>");
+        $html = str_replace("\n", ' ', $html); //replace carriage returns with spaces
+        // debug
+        if ($this->debug) {
+            echo $html;
+            exit;
+        }
+
+        $html = str_replace('&trade;', '™', $html);
+        $html = str_replace('&copy;', '©', $html);
+        $html = str_replace('&euro;', '€', $html);
+
+        $a = preg_split('/<(.*)>/U', $html, -1, PREG_SPLIT_DELIM_CAPTURE);
+        $skip = false;
+        foreach ($a as $i => $e) {
+            if (!$skip) {
+                if ($this->HREF)
+                    $e = str_replace("\n", "", str_replace("\r", "", $e));
+                if ($i % 2 == 0) {
+                    // new line
+                    if ($this->PRE)
+                        $e = str_replace("\r", "\n", $e);
+                    else
+                        $e = str_replace("\r", "", $e);
+                    //Text
+                    if ($this->HREF) {
+                        $this->PutLink($this->HREF, $e);
+                        $skip = true;
+                    } else
+                        $this->Write(5, stripslashes(txtentities($e)));
+                } else {
+                    //Tag
+                    if (substr(trim($e), 0, 1) == '/')
+                        $this->CloseTag(strtoupper(substr($e, strpos($e, '/'))));
+                    else {
+                        //Extract attributes
+                        $a2 = explode(' ', $e);
+                        $tag = strtoupper(array_shift($a2));
+                        $attr = array();
+                        foreach ($a2 as $v) {
+                            if (preg_match('/([^=]*)=["\']?([^"\']*)/', $v, $a3))
+                                $attr[strtoupper($a3[1])] = $a3[2];
+                        }
+                        $this->OpenTag($tag, $attr);
+                    }
+                }
+            } else {
+                $this->HREF = '';
+                $skip = false;
+            }
+        }
+    }
+
+    function OpenTag($tag, $attr)
+    {
+        //Opening tag
+        switch ($tag) {
+            case 'STRONG':
+            case 'B':
+                if ($this->bi)
+                    $this->SetStyle('B', true);
+                else
+                    $this->SetStyle('U', true);
+                break;
+            case 'H1':
+                $this->Ln(5);
+                $this->SetTextColor(150, 0, 0);
+                $this->SetFontSize(22);
+                break;
+            case 'H2':
+                $this->Ln(5);
+                $this->SetFontSize(18);
+                $this->SetStyle('U', true);
+                break;
+            case 'H3':
+                $this->Ln(5);
+                $this->SetFontSize(16);
+                $this->SetStyle('U', true);
+                break;
+            case 'H4':
+                $this->Ln(5);
+                $this->SetTextColor(102, 0, 0);
+                $this->SetFontSize(14);
+                if ($this->bi)
+                    $this->SetStyle('B', true);
+                break;
+            case 'PRE':
+                $this->SetFont('Courier', '', 11);
+                $this->SetFontSize(11);
+                $this->SetStyle('B', false);
+                $this->SetStyle('I', false);
+                $this->PRE = true;
+                break;
+            case 'RED':
+                $this->SetTextColor(255, 0, 0);
+                break;
+            case 'BLOCKQUOTE':
+                $this->mySetTextColor(100, 0, 45);
+                $this->Ln(3);
+                break;
+            case 'BLUE':
+                $this->SetTextColor(0, 0, 255);
+                break;
+            case 'I':
+            case 'EM':
+                if ($this->bi)
+                    $this->SetStyle('I', true);
+                break;
+            case 'U':
+                $this->SetStyle('U', true);
+                break;
+            case 'A':
+                $this->HREF = $attr['HREF'];
+                break;
+            case 'IMG':
+                if (isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
+                    if (!isset($attr['WIDTH']))
+                        $attr['WIDTH'] = 0;
+                    if (!isset($attr['HEIGHT']))
+                        $attr['HEIGHT'] = 0;
+                    $this->Image($attr['SRC'], $this->GetX(), $this->GetY(), px2mm($attr['WIDTH']), px2mm($attr['HEIGHT']));
+                    $this->Ln(3);
+                }
+                break;
+            case 'LI':
+                $this->Ln(3);
+                $this->SetTextColor(190, 0, 0);
+                $this->Write(5, utf8_decode('     » '));
+                $this->mySetTextColor(-1);
+                break;
+            case 'TR':
+                $this->Ln(7);
+                $this->PutLine();
+                break;
+            case 'BR':
+                $this->Ln(2);
+                break;
+            case 'P':
+                $this->Ln(5);
+                break;
+            case 'HR':
+                $this->PutLine();
+                break;
+            case 'FONT':
+                if (isset($attr['COLOR']) && $attr['COLOR'] != '') {
+                    $coul = hex2dec($attr['COLOR']);
+                    $this->mySetTextColor($coul['R'], $coul['G'], $coul['B']);
+                    $this->issetcolor = true;
+                }
+                if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
+                    $this->SetFont(strtolower($attr['FACE']));
+                    $this->issetfont = true;
+                }
+                break;
+        }
+    }
+
+    function CloseTag($tag)
+    {
+        //Closing tag
+        if ($tag == 'H1' || $tag == 'H2' || $tag == 'H3' || $tag == 'H4') {
+            $this->Ln(6);
+            $this->SetFont('Times', '', 12);
+            $this->SetFontSize(12);
+            $this->SetStyle('U', false);
+            $this->SetStyle('B', false);
+            $this->mySetTextColor(-1);
+        }
+        if ($tag == 'PRE') {
+            $this->SetFont('Times', '', 12);
+            $this->SetFontSize(12);
+            $this->PRE = false;
+        }
+        if ($tag == 'RED' || $tag == 'BLUE')
+            $this->mySetTextColor(-1);
+        if ($tag == 'BLOCKQUOTE') {
+            $this->mySetTextColor(0, 0, 0);
+            $this->Ln(3);
+        }
+        if ($tag == 'STRONG')
+            $tag = 'B';
+        if ($tag == 'EM')
+            $tag = 'I';
+        if ((!$this->bi) && $tag == 'B')
+            $tag = 'U';
+        if ($tag == 'B' || $tag == 'I' || $tag == 'U')
+            $this->SetStyle($tag, false);
+        if ($tag == 'A')
+            $this->HREF = '';
+        if ($tag == 'FONT') {
+            if ($this->issetcolor == true) {
+                $this->SetTextColor(0, 0, 0);
+            }
+            if ($this->issetfont) {
+                $this->SetFont('Times', '', 12);
+                $this->issetfont = false;
+            }
+        }
+    }
+
+    function SetStyle($tag, $enable)
+    {
+        $this->$tag += ($enable ? 1 : -1);
+        $style = '';
+        foreach (array('B', 'I', 'U') as $s) {
+            if ($this->$s > 0)
+                $style .= $s;
+        }
+        $this->SetFont('', $style);
+    }
+
+    function PutLink($URL, $txt)
+    {
+        //Put a hyperlink
+        $this->SetTextColor(0, 0, 255);
+        $this->SetStyle('U', true);
+        $this->Write(5, $txt, $URL);
+        $this->SetStyle('U', false);
+        $this->mySetTextColor(-1);
+    }
+
+    function PutLine()
+    {
+        $this->Ln(2);
+        $this->Line($this->GetX(), $this->GetY(), $this->GetX() + 187, $this->GetY());
+        $this->Ln(3);
+    }
+
+    function mySetTextColor($r, $g = 0, $b = 0)
+    {
+        static $_r = 0, $_g = 0, $_b = 0;
+
+        if ($r == -1)
+            $this->SetTextColor($_r, $_g, $_b);
+        else {
+            $this->SetTextColor($r, $g, $b);
+            $_r = $r;
+            $_g = $g;
+            $_b = $b;
+        }
+    }
+
+    function PutMainTitle($title)
+    {
+        if (strlen($title) > 55)
+            $title = substr($title, 0, 55) . "...";
+        $this->SetTextColor(33, 32, 95);
+        $this->SetFontSize(20);
+        $this->SetFillColor(255, 204, 120);
+        $this->Cell(0, 20, $title, 1, 1, "C", 1);
+        $this->SetFillColor(255, 255, 255);
+        $this->SetFontSize(12);
+        $this->Ln(5);
+    }
+
+    function PutMinorHeading($title)
+    {
+        $this->SetFontSize(12);
+        $this->Cell(0, 5, $title, 0, 1, "C");
+        $this->SetFontSize(12);
+    }
+
+    function PutMinorTitle($title, $url = '')
+    {
+        $title = str_replace('http://', '', $title);
+        if (strlen($title) > 70)
+            if (!(strrpos($title, '/') == false))
+                $title = substr($title, strrpos($title, '/') + 1);
+        $title = substr($title, 0, 70);
+        $this->SetFontSize(16);
+        if ($url != '') {
+            $this->SetStyle('U', false);
+            $this->SetTextColor(0, 0, 180);
+            $this->Cell(0, 6, $title, 0, 1, "C", 0, $url);
+            $this->SetTextColor(0, 0, 0);
+            $this->SetStyle('U', false);
+        } else
+            $this->Cell(0, 6, $title, 0, 1, "C", 0);
+        $this->SetFontSize(12);
+        $this->Ln(4);
+    }
+}//end of class
