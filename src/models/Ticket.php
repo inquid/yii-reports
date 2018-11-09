b389e8daaba3751c0145e2d8b6a678395023e6a6
Luis Gonzalez <luisarmando1234@gmail.com>
2018-11-09 14:45:27 -0600

    Update Ticket.php

--- src/models/Ticket.php
+++ src/models/Ticket.php
@@ -1,1 +1,13 @@
+<?php
+namespace inquid\yiireports\models;
 
+use yii\base\Model;
+
+class Ticket extends Model
+{
+    public $title;
+    public $beforeData = [];
+    public $tableHeaders = [];
+    public $tableData = [];
+    public $postData;
+}
